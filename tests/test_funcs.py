4c3f4b3effa681fe156a183c7962598bff49b0a6
pecheritsa <pecheritsa.roman@yandex.ru>
2024-05-26 13:49:25 +0300

    test_funcs -> Добавлено тестирование функции dtr_to_str

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,6 +1,6 @@
 import pytest
 from datetime import datetime
-from coursework3.funcs import correct_data, json_to_list, five_operations
+from coursework3.funcs import correct_data, json_to_list, five_operations, dt_to_str
 
 
 @pytest.mark.parametrize('list_dict, expected', [
@@ -29,3 +29,9 @@ def test_five_operations():
         {'date': 0},
         {'date': 4}
     ]) == [{'date': 11}, {'date': 4}, {'date': 3}, {'date': 2}, {'date': 0}]
+
+
+def test_dt_to_string():
+    with pytest.raises(AttributeError):
+        dt_to_str('mb_error?')
+    assert dt_to_str(datetime.fromisoformat("2019-04-04T23:20:05.206878")) == '04.04.2019'
