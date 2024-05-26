c96ae0ce4a488dc0688eeeb985169f4d29c96c02
pecheritsa <pecheritsa.roman@yandex.ru>
2024-05-26 13:37:27 +0300

    main -> Функция main -> Добавлена логика получения даты, источника получения и передачи средств

--- coursework3/main.py
+++ coursework3/main.py
@@ -1,10 +1,14 @@
-from funcs import json_to_list, correct_data, five_operations
+from funcs import json_to_list, correct_data, five_operations, dt_to_str, masked
 
 
 def main():
     data = json_to_list()
     correct = correct_data(data)
     operations = five_operations(correct)
+    for op in operations:
+        date = dt_to_str(op.get('date'))
+        source_from = masked(op.get('from'))
+        source_to = masked(op.get('to'))
 
 
 if __name__ == '__main__':
