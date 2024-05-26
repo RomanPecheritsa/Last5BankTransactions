1e46e5ddf37e9eb8aac1bccb07613e7d3707031a
pecheritsa <pecheritsa.roman@yandex.ru>
2024-05-26 11:45:33 +0300

    test_funcs -> Добавлено тестирование функций correct_data, json_to list

---
+++ tests/test_funcs.py
@@ -0,0 +1,19 @@
+import pytest
+from datetime import datetime
+from coursework3.funcs import correct_data, json_to_list
+
+
+@pytest.mark.parametrize('list_dict, expected', [
+    ([{}], []),
+    ([{"date": "2019-04-04T23:20:05.206878", "state": "EXECUTED"}],
+     [{"date": datetime.fromisoformat("2019-04-04T23:20:05.206878"), "state": "EXECUTED"}]),
+    ([{"date": "2019-04-04T23:20:05.206878"}], []),
+    ([{"state": "EXECUTED"}], []),
+])
+def test_correct_data(list_dict, expected):
+    assert correct_data(list_dict) == expected
+
+
+def test_correct_json():
+    with pytest.raises(FileNotFoundError):
+        json_to_list('egegeg/error.txt')
