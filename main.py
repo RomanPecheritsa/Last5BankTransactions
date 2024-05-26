627b3386d81f74e8f270b25a0a17c040a80991b6
pecheritsa <pecheritsa.roman@yandex.ru>
2024-05-26 14:15:42 +0300

    main -> Добавлена операция получения типа операции, добавлена логика вывода информации в терминал

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ def main():
     operations = five_operations(correct)
     for op in operations:
         date = dt_to_str(op.get('date'))
+        desc = op.get('description')
         source_from = masked(op.get('from'))
         source_to = masked(op.get('to'))
         amount_currency = transfer_amount_currency(op)
+        print(f'{date} {desc}\n{source_from} -> {source_to}\n{amount_currency}')
+        print()
 
 
 if __name__ == '__main__':
