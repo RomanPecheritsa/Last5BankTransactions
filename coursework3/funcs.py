bca300132f002545aa7d8f18a1d99f186370d2be
pecheritsa <pecheritsa.roman@yandex.ru>
2024-05-26 11:30:42 +0300

    funcs -> Добавлена функция correct_data

--- coursework3/funcs.py
+++ coursework3/funcs.py
@@ -1,9 +1,19 @@
 import json
 from os.path import abspath
+from datetime import datetime
 
 
 def json_to_list(path=abspath('../data/operations.json')):
     with open(path, encoding='utf-8') as file:
         data = json.load(file)
     return data
+
+
+def correct_data(data):
+    correct = []
+    for obj in data:
+        if obj and obj.get('date') is not None and obj.get('state') == 'EXECUTED':
+            obj['date'] = datetime.fromisoformat(obj['date'])
+            correct.append(obj)
+    return correct
 
