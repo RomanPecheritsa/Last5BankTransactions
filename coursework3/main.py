d98a57caafea5eec8fe1449d658d6149a46f4006
pecheritsa <pecheritsa.roman@yandex.ru>
2024-05-26 11:54:30 +0300

    main -> Функция main -> Добавлена операция получения 5 последних транзакций

--- coursework3/main.py
+++ coursework3/main.py
@@ -1,9 +1,11 @@
-from funcs import json_to_list, correct_data
+from funcs import json_to_list, correct_data, five_operations
 
 
 def main():
     data = json_to_list()
     correct = correct_data(data)
+    operations = five_operations(correct)
+
 
 if __name__ == '__main__':
     main()
