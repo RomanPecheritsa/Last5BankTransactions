a6d75208a3551fc62a05fdea2da0b256d66fdaf0
pecheritsa <pecheritsa.roman@yandex.ru>
2024-05-26 11:58:00 +0300

    test_funcs -> Добавлено тестирование функции five_operations

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,6 +1,6 @@
 import pytest
 from datetime import datetime
-from coursework3.funcs import correct_data, json_to_list
+from coursework3.funcs import correct_data, json_to_list, five_operations
 
 
 @pytest.mark.parametrize('list_dict, expected', [
@@ -17,3 +17,15 @@ def test_correct_data(list_dict, expected):
 def test_json_to_list():
     with pytest.raises(FileNotFoundError):
         json_to_list('egegeg/error.txt')
+
+
+def test_five_operations():
+    assert five_operations([]) == []
+    assert five_operations([
+        {'date': 2},
+        {'date': 3},
+        {'date': -5},
+        {'date': 11},
+        {'date': 0},
+        {'date': 4}
+    ]) == [{'date': 11}, {'date': 4}, {'date': 3}, {'date': 2}, {'date': 0}]
